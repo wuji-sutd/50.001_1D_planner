3bac8ab842eb2936a25c952815d2d780812c6e5a
jeanmah <jeanmah.828@gmail.com>
2020-11-14 00:47:28 +0800

    empty project

---
+++ 50001_1D_planner/app/src/main/java/com/example/a50001_1d_planner/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a50001_1d_planner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
